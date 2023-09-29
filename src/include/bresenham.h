5ad1fceaa566107a95da39ee8c0cc4f590de7d24
8dcc <8dcc.git@gmail.com>
2023-09-29 22:18:17 +0200

    Add raycast functions

--- src/include/bresenham.h
+++ src/include/bresenham.h
@@ -4,5 +4,6 @@
 #define ABS(n) ((n) < 0 ? (-(n)) : (n))
 
 void bresenham_line(int x0, int y0, int x1, int y1);
+void raycast_line(int x0, int y0, int x1, int y1, int* out_x, int* out_y);
 
 #endif /* BRESENHAM_H_ */
