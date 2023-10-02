9bdc5052989d1bb8294697749e6ed88731decc9c
8dcc <8dcc.git@gmail.com>
2023-10-02 20:17:07 +0200

    Add draw_line_and_fill, use for draw_angle
    Add draw_raycast_and_fill, use for draw_raycast_angle (not perfect)
    Add surrounded() inline function

--- src/draw.c
+++ src/draw.c
@@ -5,14 +5,17 @@
  */
 
 #include <stdint.h>
+#include <stdbool.h>
 #include "include/main.h"
 #include "include/draw.h"
 #include "include/raycast.h"
 #include "include/rotation.h"
 
+#define DRAW_ANGLE_STEP DEG2RAD(0.1f)
+
 /* Used by draw_angle() and modified by the draw_raycast_angle() inline wrapper
  * in bresenham.h */
-drawfunc_t fnDrawLine = &draw_line;
+drawfunc_t fnDrawLine = &draw_line_and_fill;
 
 /*----------------------------------------------------------------------------*/
 
@@ -70,12 +73,113 @@ void draw_line(vec2_t a, vec2_t b, color_t col) {
     }
 }
 
+/*----------------------------------------------------------------------------*/
+
+static inline bool surrounded(int y, int x, color_t col) {
+    /*
+     *    | ? |
+     * ---+---+---
+     *  ? | X | ?
+     * ---+---+---
+     *    | ? |
+     */
+    return (get_val(y - 1, x) == col) && (get_val(y + 1, x) == col) &&
+           (get_val(y, x + 1) == col) && (get_val(y, x + 1) == col);
+}
+
+static void bresenham_x_fill(vec2_t a, vec2_t b, color_t col) {
+    int dx = b.x - a.x;
+    int dy = (b.y > a.y) ? b.y - a.y : a.y - b.y;
+
+    int diff   = 2 * dy - dx;
+    int y_step = (b.y > a.y) ? 1 : -1;
+
+    int y = a.y;
+    for (int x = a.x; x <= b.x; x++) {
+        set_val(y, x, col);
+
+        /* Check adjactent cells. If there is an empty one, and it's also
+         * surrounded by filled ones, fill it. */
+        if (y >= 2 && y < ARR_H - 2 && x >= 2 && x < ARR_W - 2) {
+            if (surrounded(y - 1, x, col))
+                set_val(y - 1, x, col);
+            if (surrounded(y + 1, x, col))
+                set_val(y + 1, x, col);
+            if (surrounded(y, x - 1, col))
+                set_val(y, x - 1, col);
+            if (surrounded(y, x + 1, col))
+                set_val(y, x + 1, col);
+        }
+
+        if (diff > 0) {
+            diff += 2 * (dy - dx);
+            y += y_step;
+        } else {
+            diff += 2 * dy;
+        }
+    }
+}
+
+static void bresenham_y_fill(vec2_t a, vec2_t b, color_t col) {
+    int dx = (b.x > a.x) ? b.x - a.x : a.x - b.x;
+    int dy = b.y - a.y;
+
+    int diff   = 2 * dx - dy;
+    int x_step = (b.x > a.x) ? 1 : -1;
+
+    int x = a.x;
+    for (int y = a.y; y <= b.y; y++) {
+        set_val(y, x, col);
+
+        /* Check adjactent cells. If there is an empty one, and it's also
+         * surrounded by filled ones, fill it. */
+        if (y >= 2 && y < ARR_H - 2 && x >= 2 && x < ARR_W - 2) {
+            if (surrounded(y - 1, x, col))
+                set_val(y - 1, x, col);
+            if (surrounded(y + 1, x, col))
+                set_val(y + 1, x, col);
+            if (surrounded(y, x - 1, col))
+                set_val(y, x - 1, col);
+            if (surrounded(y, x + 1, col))
+                set_val(y, x + 1, col);
+        }
+
+        if (diff > 0) {
+            diff += 2 * (dx - dy);
+            x += x_step;
+        } else {
+            diff += 2 * dx;
+        }
+    }
+}
+
+void draw_line_and_fill(vec2_t a, vec2_t b, color_t col) {
+    if (ABS(b.y - a.y) < ABS(b.x - a.x)) {
+        if (b.x >= a.x)
+            bresenham_x_fill(a, b, col);
+        else
+            bresenham_x_fill(b, a, col);
+    } else {
+        if (b.y >= a.y)
+            bresenham_y_fill(a, b, col);
+        else
+            bresenham_y_fill(b, a, col);
+    }
+}
+
+/*----------------------------------------------------------------------------*/
+
 void draw_raycast(vec2_t start, vec2_t end, color_t col) {
     vec2_t cast = raycast(start, end);
     draw_line(start, cast, col);
 }
 
-#define DRAW_ANGLE_STEP DEG2RAD(0.1f)
+void draw_raycast_and_fill(vec2_t start, vec2_t end, color_t col) {
+    vec2_t cast = raycast(start, end);
+    draw_line_and_fill(start, cast, col);
+}
+
+/*----------------------------------------------------------------------------*/
 
 void draw_angle(float deg, vec2_t vertex, vec2_t ang_center, color_t col) {
     /* Start and end points of our angle */
--- src/include/draw.h
+++ src/include/draw.h
@@ -1,38 +1,74 @@
 #ifndef DRAW_H_
 #define DRAW_H_
 
+/* Declared in bresenham.c */
+typedef void (*drawfunc_t)(vec2_t a, vec2_t b, color_t col);
+extern drawfunc_t fnDrawLine;
+
 /**
  * @brief Draw a line in the global array using the bresenham's line algorithm
  * @details Uses the set_val inline function from main.h
  * @param[in] a Start of line
  * @param[in] b End of line
+ * @param[in] col Color of the line
  */
 void draw_line(vec2_t a, vec2_t b, color_t col);
+
+/**
+ * @brief Draw a line in the global array and fill empty gaps in adjacent cells
+ * @details While drawing the line from A to B, it will check if the adjacent
+ * tiles are empty, and if they are, it will check if they are surrounded by
+ * filled cells. If it is, it will fill it as well.
+ *
+ * Useful for filling empty gaps in draw_angle()
+ * @param[in] a Start of line
+ * @param[in] b End of line
+ * @param[in] col Color of the line
+ */
+void draw_line_and_fill(vec2_t a, vec2_t b, color_t col);
 
 /**
  * @brief Cast a ray from start to end, and draw a line to the returned value
  * @details Uses the raycast() function
  * @param[in] start Start of ray
  * @param[in] end End of ray
+ * @param[in] col Color of the line
  */
 void draw_raycast(vec2_t start, vec2_t end, color_t col);
 
-/* Declared in bresenham.c */
-typedef void (*drawfunc_t)(vec2_t a, vec2_t b, color_t col);
-extern drawfunc_t fnDrawLine;
+/**
+ * @brief Cast a ray from start to end, draw a line to the returned value, and
+ * fill adjacent empty cells
+ * @details Same as draw_raycast() but uses draw_line_and_fill() instead of
+ * draw_line()
+ * @param[in] start Start of ray
+ * @param[in] end End of ray
+ * @param[in] col Color of the line
+ */
+void draw_raycast_and_fill(vec2_t start, vec2_t end, color_t col);
 
 /**
  * @brief Draws an angle cone of X degrees from an origin to another point
  * @param[in] deg Angle to draw in degrees, not radians
  * @param[in] vertex Vertex of the angle
  * @param[in] ang_center The position at the center of the angle
+ * @param[in] col Color of the line
  */
 void draw_angle(float deg, vec2_t vertex, vec2_t ang_center, color_t col);
 
+/**
+ * @brief Raycasts an angle cone of X degrees from an origin to another point
+ * @defails Same as draw_angle() but uses draw_raycast() instead of
+ * draw_line_and_fill()
+ * @param[in] deg Angle to draw in degrees, not radians.
+ * @param[in] vertex Vertex of the angle
+ * @param[in] ang_center The position at the center of the angle
+ * @param[in] col Color of the line
+ */
 static inline void draw_raycast_angle(float deg, vec2_t vertex,
                                       vec2_t ang_center, color_t col) {
     drawfunc_t old = fnDrawLine;
-    fnDrawLine     = draw_raycast;
+    fnDrawLine     = draw_raycast_and_fill;
     draw_angle(deg, vertex, ang_center, col);
     fnDrawLine = old;
 }
